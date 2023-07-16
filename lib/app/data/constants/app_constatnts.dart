4141bf8cd794bafc0e7d2fbb6297ec7f51059086
Sanjay Mehra <mehra271198@gmail.com>
2023-07-16 18:11:22 +0530

    push notification added

--- lib/app/data/constants/app_constatnts.dart
+++ lib/app/data/constants/app_constatnts.dart
@@ -1,4 +1,5 @@
 // Local Storage Keys
 const String kToken = 'token';
+const String kDeviceToken = 'deviceToken';
 const String kUserId = 'userId';
 const String baseAPIURL = 'http://18.133.142.150:9000/';
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -6,10 +7,13 @@ import 'package:restaurant_app/app/data/constants/app_constatnts.dart';
 import 'package:restaurant_app/app/modules/login/bindings/login_binding.dart';
 
 import 'app/routes/app_pages.dart';
+import 'app/services/push_notification_service.dart';
 
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  await PushNotificationService().setupInteractedMessage();
+
   runApp(
     GetMaterialApp(
       title: "Hamilton Restaurant App",
@@ -20,4 +24,9 @@ Future<void> main() async {
       debugShowCheckedModeBanner: false,
     ),
   );
+  RemoteMessage? initialMessage =
+      await FirebaseMessaging.instance.getInitialMessage();
+  if (initialMessage != null) {
+    // App received a notification when it was killed
+  }
 }
